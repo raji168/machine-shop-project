3a82ed2deb43717de7612c4f836475d45995e017
raji <rajeswarisundar99@gmail.com>
2021-12-24 17:32:14 +0530

    userapi

--- src/app/services/user-api.service.ts
+++ src/app/services/user-api.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getShiftAll() {
+    const url = ` http://192.168.0.13:3002/shifts`;
+    return this.http.get<User[]>(url);
+  }
 }
