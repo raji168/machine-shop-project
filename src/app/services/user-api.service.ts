e3b17d335823dc2febbf7fff587eaa8fcea250f3
raji <rajeswarisundar99@gmail.com>
2021-12-27 11:49:04 +0530

    ad1

--- src/app/services/user-api.service.ts
+++ src/app/services/user-api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from '../models/user.model';
 
 @Injectable({
@@ -8,6 +9,26 @@ import { User } from '../models/user.model';
 export class UserApiService {
 
   constructor(private http: HttpClient) { }
+
+  userForm: FormGroup = new FormGroup({
+    serialNo: new FormControl(''),
+    name : new FormControl(''),
+    role : new FormControl(''),
+    email : new FormControl(''),
+    contactNo : new FormControl(''),
+    userName : new FormControl(''),
+  });
+   
+  initializeFormGroup(){
+    this.userForm.setValue({
+      serialNo: '',
+      name: '',
+      role: '',
+      email: '',
+      contactNo:'',
+      userName: ''
+    });
+  }
 
   getShiftAll() {
     const url = ` http://192.168.0.13:3002/shifts`;
