70d8cb81f1140b8306812f9b2577ad6b3e1894d4
prabha <swarnaprabhamaha@gmail.com>
2021-12-30 18:14:20 +0530

    commit changes

--- src/app/master/user/add-user/add-user.component.ts
+++ src/app/master/user/add-user/add-user.component.ts
@@ -14,6 +14,7 @@ import { User } from 'src/app/models/user.model';
   styleUrls: ['./add-user.component.scss']
 })
 export class AddUserComponent implements OnInit {
+  
   userForm: FormGroup;
 
   dataRole :Role[] =[];
