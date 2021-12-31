48abe0e6db608e43b9353d5385638e9f64f43a4f
arun <arunkumar.akr1@gmail.com>
2021-12-31 15:38:52 +0530

    role done commit

--- src/app/master/user/add-user/add-user.component.ts
+++ src/app/master/user/add-user/add-user.component.ts
@@ -36,8 +36,8 @@ export class AddUserComponent implements OnInit {
     });
 
     if (this.user) {
-      this.userService.userForm.patchValue(this.user);
-      this.userService.userForm.get('role')?.setValue(this.user.role._id);
+      this.userService.userForm.patchValue(this.data.user);
+      this.userService.userForm.get('role')?.setValue(this.data.user.role._id);
     }
 
   }
