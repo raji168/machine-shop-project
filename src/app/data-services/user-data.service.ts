2506e154a636547e34b3accb489837394f79a009
prabha <swarnaprabhamaha@gmail.com>
2022-01-06 10:08:00 +0530

    commit user changes

--- src/app/data-services/user-data.service.ts
+++ src/app/data-services/user-data.service.ts
@@ -35,39 +35,39 @@ export class UserDataService {
   constructor() { }
 }
 
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-import { User } from '../models/user.model';
+// import { Injectable } from '@angular/core';
+// import { Subject } from 'rxjs';
+// import { User } from '../models/user.model';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class UserDataService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class UserDataService {
 
-  private users: User[] =[]
+//   private users: User[] =[]
   
-  userUpdated$ = new Subject<User[]>()
+//   userUpdated$ = new Subject<User[]>()
 
-  getUsers() {
-    return[...this.users]
-  }
+//   getUsers() {
+//     return[...this.users]
+//   }
 
-  loadUsers(users: User[]) {
-    this.users = users;
-  }
+//   loadUsers(users: User[]) {
+//     this.users = users;
+//   }
 
-  addUser(user : User) {
-    this.users = [...this.users, user]
-    this.userUpdated$.next(this.users);
-  }
+//   addUser(user : User) {
+//     this.users = [...this.users, user]
+//     this.userUpdated$.next(this.users);
+//   }
 
-  updateUser(userResponse: User) {
-    const updateUser = this.users.find(user => user._id === user._id)
-    const updateUserIndex = this.users.findIndex(user => user._id === user._id)
-    const updatedUser = {...updateUser, ...userResponse}
-    this.users[updateUserIndex] = updatedUser
-    this.userUpdated$.next(this.users);
-  }
-  constructor() { }
-}
+//   updateUser(userResponse: User) {
+//     const updateUser = this.users.find(user => user._id === user._id)
+//     const updateUserIndex = this.users.findIndex(user => user._id === user._id)
+//     const updatedUser = {...updateUser, ...userResponse}
+//     this.users[updateUserIndex] = updatedUser
+//     this.userUpdated$.next(this.users);
+//   }
+//   constructor() { }
+// }
 
