6b05cb3de8f1d458da43cd246551587faa2526b2
prabha <swarnaprabhamaha@gmail.com>
2022-01-06 10:14:50 +0530

    commit changes

--- src/app/data-services/user-data.service.ts
+++ src/app/data-services/user-data.service.ts
@@ -1,6 +1,6 @@
 
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { User } from '../models/user.model';
 
 @Injectable({
@@ -10,7 +10,7 @@ export class UserDataService {
 
   private users: User[] =[]
   
-  userUpdated$ = new BehaviorSubject<User[]>([])
+  userUpdated$ = new Subject<User[]>()
 
   getUsers() {
     return[...this.users]
@@ -18,7 +18,6 @@ export class UserDataService {
 
   loadUsers(users: User[]) {
     this.users = users;
-    this.userUpdated$.next(this.users)
   }
 
   addUser(user : User) {
@@ -31,48 +30,8 @@ export class UserDataService {
     const updateUserIndex = this.users.findIndex(user => user._id === user._id)
     const updatedUser = {...updateUser, ...userResponse}
     this.users[updateUserIndex] = updatedUser
-    console.log(this.users)
     this.userUpdated$.next(this.users);
   }
-  deleteUser(id:string) {
-    this.users = this.users.filter(user => user._id !== id);
-    this.userUpdated$.next(this.users);
-  }
+  constructor() { }
 }
-
-// import { Injectable } from '@angular/core';
-// import { Subject } from 'rxjs';
-// import { User } from '../models/user.model';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class UserDataService {
-
-//   private users: User[] =[]
-  
-//   userUpdated$ = new Subject<User[]>()
-
-//   getUsers() {
-//     return[...this.users]
-//   }
-
-//   loadUsers(users: User[]) {
-//     this.users = users;
-//   }
-
-//   addUser(user : User) {
-//     this.users = [...this.users, user]
-//     this.userUpdated$.next(this.users);
-//   }
-
-//   updateUser(userResponse: User) {
-//     const updateUser = this.users.find(user => user._id === user._id)
-//     const updateUserIndex = this.users.findIndex(user => user._id === user._id)
-//     const updatedUser = {...updateUser, ...userResponse}
-//     this.users[updateUserIndex] = updatedUser
-//     this.userUpdated$.next(this.users);
-//   }
-//   constructor() { }
-// }
 
