3eebc573b4fc8fc3b0fd22394ac2f9ea6831f947
raji <rajeswarisundar99@gmail.com>
2022-01-10 12:21:38 +0530

    lazyloading  commit

--- src/app/master/master.module.ts
+++ src/app/master/master.module.ts
@@ -4,24 +4,12 @@ import {MatButtonModule} from '@angular/material/button';
 import { MaterialModule } from "../material/material.module";
 import { MasterRoutingModule } from "./master-routing.module";
 import { MasterComponent } from "./master.component";
-import { RoleComponent } from './role/role.component';
 import { ReactiveFormsModule } from "@angular/forms";
-import { InstrumentComponent } from './instrument/instrument.component';
-import { UserComponent } from './user/user.component';
-import { AddUserComponent } from './user/add-user/add-user.component';
-import { AddInstrumentComponent } from './instrument/add-instrument/add-instrument.component';
-import { AddRoleComponent } from './role/add-role/add-role.component';
 
 @NgModule({
     declarations: [
 
         MasterComponent,
-        RoleComponent,
-        InstrumentComponent,
-        UserComponent,
-        AddUserComponent,
-        AddInstrumentComponent,
-        AddRoleComponent,
     ],
     imports: [
         CommonModule,
@@ -29,7 +17,6 @@ import { AddRoleComponent } from './role/add-role/add-role.component';
         ReactiveFormsModule,
         MasterRoutingModule,
         MatButtonModule
-        
     ],
 })
 
