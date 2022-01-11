9f0c7916abb9b56c1d72ceef863dd65df772b5fe
raji <rajeswarisundar99@gmail.com>
2022-01-11 16:25:24 +0530

    productlibrary added commit

--- src/app/main/master/user/user.module.ts
+++ src/app/main/master/user/user.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/app/master/user/user.module.ts
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
@@ -21,26 +20,4 @@ import { UserComponent } from "./user.component";
         FormsModule
     ]
 })
-=======
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
-import { MaterialModule } from "src/app/material/material.module";
-import { AddUserComponent } from "./add-user/add-user.component";
-import { UserRoutinModule } from "./user-routing.module";
-import { UserComponent } from "./user.component";
-
-@NgModule({
-    declarations: [
-        UserComponent,
-        AddUserComponent
-    ],
-    imports: [
-        CommonModule,
-        ReactiveFormsModule,
-        MaterialModule,
-        UserRoutinModule
-    ]
-})
->>>>>>> 1d72636d8662bd65054bda8b77705be574cf4447:src/app/main/master/user/user.module.ts
 export class UserModule { }
