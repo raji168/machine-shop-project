b260109659171762f4ab66b0450745722c5a480d
raji <rajeswarisundar99@gmail.com>
2022-01-12 13:18:16 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,14 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainComponent } from './main/main.component';
-import { ProductLibraryComponent } from './main/productlibrary/productlibrary.component';
+
 const routes: Routes = [
 
   { path: '', redirectTo: 'main/master', pathMatch: 'full' },
   {
-    path: '', component: MainComponent, children: [
-      {
-        path: 'master', loadChildren: () => import('./main/main.module').then(m => m.MainModule),
-      }
-    ]
-  },
-{ path :'product-library', component:ProductLibraryComponent}
+    path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule)
+  }
+
+
 ];
 
 @NgModule({
