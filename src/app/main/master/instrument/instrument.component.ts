a5afc89f66c4f163f90c53dcd10a5a7f78e32eb7
prabha <swarnaprabhamaha@gmail.com>
2022-01-12 12:42:16 +0530

    commit

--- src/app/main/master/instrument/instrument.component.ts
+++ src/app/main/master/instrument/instrument.component.ts
@@ -73,10 +73,6 @@ export class InstrumentComponent implements OnInit {
   }
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 3a9efad18a43533b7c947eb1f5f8c8447fcb52cd
   fillGrid() {
     this.instrumentService.get()
       .subscribe(
@@ -87,31 +83,9 @@ export class InstrumentComponent implements OnInit {
 
         }
       );
-<<<<<<< HEAD
-    // this.instrumentService.get()
-    //   .subscribe(
-    //     data => {
-    // this.grdlistData = new MatTableDataSource();
-    // this.instrumentDataSource$.subscribe(
-    //   ((res) => {
-    //     this.grdlistData = res.data;
-    //     this.grdlistData = new MatTableDataSource(res.data);
-    //     this.grdlistData.sort = this.sort;
-    //     this.grdlistData.paginator = this.paginator;
-    //   })
-    // )
-    //   }
-    // );
-
-    // }
-  }
-
-  ngAfterViewInit(): void {
-=======
 
       }
   ngAfterViewInit(): void{
->>>>>>> 3a9efad18a43533b7c947eb1f5f8c8447fcb52cd
     this.grdlistData.paginator = this.paginator;
     this.grdlistData.sort = this.sort;
   }
@@ -125,66 +99,9 @@ export class InstrumentComponent implements OnInit {
     this.applyFilter();
   }
 
-<<<<<<< HEAD
-  //   onSelect($event) {
-  //     const id = $event.target.value;
-  //     const isChecked = $event.target.checked;
-  //     this.grdlistData = this.grdlistData.map((d) => {
-  //     if (d.id == id) {
-  //       d.select = isChecked;
-  //       this.headerSelector = false;
-  //       return d;
-  //     }
-  //     if (id == -1) {
-  //       d.select = this.headerSelector;
-  //       return d;
-  //     }
-  //     return d;
-  //   });
-  //   console.log(this.grdlistData);
-  // }
 
-=======
-//   onSelect($event) {
-//     const id = $event.target.value;
-//     const isChecked = $event.target.checked;
-//     this.grdlistData = this.grdlistData.map((d) => {
-//     if (d.id == id) {
-//       d.select = isChecked;
-//       this.headerSelector = false;
-//       return d;
-//     }
-//     if (id == -1) {
-//       d.select = this.headerSelector;
-//       return d;
-//     }
-//     return d;
-//   });
-//   console.log(this.grdlistData);
-// }
 
-allComplete: boolean = false;
-
-updateAllComplete() {
-  this.allComplete = this.grdlistData != null && this.grdlistData.every(t => t.completed);
-}
-
-someComplete(): boolean {
-  if (this.grdlistData == null) {
-    return false;
-  }
-  return this.grdlistData.filter(t => t.completed).length > 0 && !this.allComplete;
-}
-
-setAll(completed: boolean) {
-  this.allComplete = completed;
-  if (this.grdlistData == null) {
-    return;
-  }
-  this.grdlistData.forEach(t => (t.completed = completed));
-}
   
->>>>>>> 3a9efad18a43533b7c947eb1f5f8c8447fcb52cd
   onCreate() {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
