84c8752382306bfde02dd4c8cd54a2cd8200d2a7
prabha <swarnaprabhamaha@gmail.com>
2022-01-22 12:55:31 +0530

    commit changes

--- src/app/main/productlibrary/mapping/mapping.component.ts
+++ src/app/main/productlibrary/mapping/mapping.component.ts
@@ -16,13 +16,7 @@ import { MatSort } from '@angular/material/sort';
   templateUrl: './mapping.component.html',
   styleUrls: ['./mapping.component.scss'],
 
-//   animations:[
-//     trigger('detailExpand',[
-//       state('collapsed', style({height:'0',minHeight:'0'})),
-//       state('expanded', style({height:'*'})),
-//       transition('expanded <=> collapsed' , animate('225ms cublic-bezier(0.4,0.0,0.2,1)')),
-//     ]),
-//   ],
+
 animations: [
   trigger('detailExpand', [
     state('collapsed', style({height: '0px', minHeight: '0'})),
@@ -35,247 +29,121 @@ animations: [
 
 export class MappingComponent {
 
-  // @ViewChild(MatAccordion) accordion: MatAccordion;
+  
+// dataSource = ELEMENT_DATA;
+// columnsToDisplay = ['serialNo','productName','customerName', 'process', 'status'];
+// expandedElement: PeriodicElement ;
+}
 
-  // displayedColumns :string[] = ['sno','productName','customerName','process','status'];
-
-
-  // mappingDataSource$ :Observable<MatTableDataSource<Mapping>>;
-  // dataSource;
-  // constructor(
-  //   private mappingApiService : MappingApiService,
-  //   private mappingDataService : MappingDataService
-  // ) { }
-
-
-
-  // ngOnInit(): void {
-  // this.mappingDataSource$ =this.mappingDataService.mappingUpdated$.pipe(map(data =>{
-  //   return new MatTableDataSource(data);
-  // }))
-
-  //     this.mappingApiService.get().subscribe(data =>{
-  //       this.dataSource = data
-  //       console.log(this.dataSource);
-  //     })
-  // }
-
-//   @ViewChild('outerSort', { static: true }) sort: MatSort;
-//   @ViewChildren('innerSort') innerSort: QueryList<MatSort>;
-//   @ViewChildren('innerTables') innerTables: QueryList<MatTable<Address>>;
-
-//   dataSource: MatTableDataSource<User>;
-//   usersData: User[] = [];
-//   columnsToDisplay = ['name', 'email', 'phone'];
-//   innerDisplayedColumns = ['street', 'zipCode', 'city'];
-//   expandedElement: User | null;
-
-//   constructor(
-//     private cd: ChangeDetectorRef
-//   ) { }
-
-
-//   ngOnInit(): void {
-//     USERS.forEach(user => {
-//       if (user.addresses && Array.isArray(user.addresses) && user.addresses.length) {
-//         this.usersData = [...this.usersData, { ...user, addresses: new MatTableDataSource(user.addresses) }];
-//       } else {
-//         this.usersData = [...this.usersData, user];
-//       }
-//     });
-//     this.dataSource = new MatTableDataSource(this.usersData);
-//     this.dataSource.sort = this.sort;
-
-//   }
-//   toggleRow(element: User) {
-//     element.addresses && (element.addresses as MatTableDataSource<Address>).data.length ? (this.expandedElement = this.expandedElement === element ? null : element) : null;
-//     this.cd.detectChanges();
-    
-//     this.innerTables.forEach((table, index) => (table.dataSource as MatTableDataSource<Address>).sort = this.innerSort.toArray()[index]);
-//   }
-
-//   applyFilter(filterValue: string) {
-    
-//     this.innerTables.forEach((table, index) => (table.dataSource as MatTableDataSource<Address>).filter = filterValue.trim().toLowerCase());
-//   }
+// export interface PeriodicElement {
+// productName: string;
+// serialNo: number;
+// customerName :string;
+// process: number;
+// status: string;
+// description: string;
 // }
 
-
-// export interface User {
-//   name: string;
-//   email: string;
-//   phone: string;
-//   addresses?: Address[] | MatTableDataSource<Address>;
-// }
-
-// export interface Address {
-//   street: string;
-//   zipCode: string;
-//   city: string;
-// }
-
-// export interface UserDataSource {
-//   name: string;
-//   email: string;
-//   phone: string;
-//   addresses?: MatTableDataSource<Address>;
-// }
-
-// const USERS: User[] = [
-//   {
-//     name: "prabha",
-//     email: "prabha@test.com",
-//     phone: "1236549870",
-//     addresses: [
-//       {
-//         street: "Street 1",
-//         zipCode: "641032",
-//         city: "Cbe"
-//       },
-//       {
-//         street: "Street 2",
-//         zipCode: "641021",
-//         city: "Texas"
-//       }
-//     ]
-//   },
-//   {
-//     name: "raji",
-//     email: "raji@test.com",
-//     phone: "8786541234"
-//   },
-//   {
-//     name: "rajeswari",
-//     email: "rajeswari@test.com",
-//     phone: "7895284105.",
-//     addresses: [
-//       {
-//         street: "Street 5",
-//         zipCode: "641055",
-//         city: "sidco"
-//       },
-//       {
-//         street: "Street 5",
-//         zipCode: "87452",
-//         city: "cbe"
-//       }
-//     ]
-//   }
+// const ELEMENT_DATA: PeriodicElement[] = [
+// {
+//   serialNo: 1,
+//   productName: 'Product1',
+//   customerName: 'Sample',
+//   process: 9,
+//   status: 'true',
+//   description: `Hydrogen is a chemical element with status H atomic number 1. With a standard
+//       atomic process of 1.008, hydrogen is the lightest element on the periodic table.`,
+// },
+// {
+//   serialNo: 2,
+//   productName: 'Product2',
+//   customerName: 'Sample',
+//   process: 4,
+//   status: 'true',
+//   description: `Helium is a chemical element with status Hd atomic number 2. It is a
+//       colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
+//       group in the periodic table. Its boiling point is the lowest among all the elements.`,
+// },
+// {
+//   serialNo: 3,
+//   productName: 'Product3',
+//   customerName: 'Sample',
+//   process: 6,
+//   status: 'true',
+//   description: `Lithium is a chemical element with status Ld atomic number 3. It is a soft,
+//       silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
+//       lightest solid element.`,
+// },
+// {
+//   serialNo: 4,
+//   productName: 'Product4',
+//   customerName: 'Sample',
+//   process: 2,
+//   status: 'ok',
+//   description: `Beryllium is a chemical element with status Bd atomic number 4. It is a
+//       relatively rare element in the universe, usually occurring as a product of the spallation of
+//       larger atomic nuclei that have collided with cosmic rays.`,
+// },
+// {
+//   serialNo: 5,
+//   productName: 'Product5',
+//   customerName: 'Sample',
+//   process: 1,
+//   status: 'true',
+//   description: `Boron is a chemical element with status B atomic number 5. Produced entirely
+//       by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a
+//       low-abundance element in the Solar system and in the Earth's crust.`,
+// },
+// {
+//   serialNo: 6,
+//   productName: 'Product6',
+//   customerName: 'Sample',
+//   process: 7,
+//   status: 'ok',
+//   description: `Carbon is a chemical element with status C atomic number 6. It is nonmetallic
+//       and tetravalent—making four electrons available to form covalent chemical bonds. It belongs
+//       to group 14 of the periodic table.`,
+// },
+// {
+//   serialNo: 7,
+//   productName: 'Product7',
+//   customerName: 'Sample',
+//   process: 3,
+//   status: 'true',
+//   description: `Nitrogen is a chemical element with status N atomic number 7. It was first
+//       discovered and isolated by Scottish physician Daniel Rutherford in 1772.`,
+// },
+// {
+//   serialNo: 8,
+//   productName: 'Product8',
+//   customerName: 'Sample',
+//   process: 4,
+//   status: 'ok',
+//   description: `Oxygen is a chemical element with status O'true' atomic number 8. It is a member of
+//        the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
+//        agent that readily forms oxides with most elements as well as with other compounds.`,
+// },
+// {
+//   serialNo: 9,
+//   productName: 'Product9',
+//   customerName: 'Sample',
+//   process: 8,
+//   status: 'true',
+//   description: `Fluorine is a chemical element with status F'true' atomic number 9. It is the
+//       lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard
+//       conditions.`,
+// },
+// {
+//   serialNo: 10,
+//   productName: 'Product10',
+//   customerName: 'Sample',
+//   process: 2,
+//   status: 'ok',
+//   description: `Neon is a chemical element with status N'true'd atomic number 10. It is a noble gas.
+//       Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about
+//       two-thirds the density of air.`,
+// },
 // ];
-dataSource = ELEMENT_DATA;
-columnsToDisplay = ['serialNo','productName','customerName', 'weight', 'symbol'];
-expandedElement: PeriodicElement | null;
-}
-
-export interface PeriodicElement {
-productName: string;
-serialNo: number;
-customerName :string;
-weight: number;
-symbol: string;
-description: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-{
-  serialNo: 1,
-  productName: 'Hydrogen',
-  customerName: 'Sample',
-  weight: 1.0079,
-  symbol: 'H',
-  description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-      atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`,
-},
-{
-  serialNo: 2,
-  productName: 'Helium',
-  customerName: 'Sample',
-  weight: 4.0026,
-  symbol: 'He',
-  description: `Helium is a chemical element with symbol He and atomic number 2. It is a
-      colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
-      group in the periodic table. Its boiling point is the lowest among all the elements.`,
-},
-{
-  serialNo: 3,
-  productName: 'Lithium',
-  customerName: 'Sample',
-  weight: 6.941,
-  symbol: 'Li',
-  description: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
-      silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
-      lightest solid element.`,
-},
-{
-  serialNo: 4,
-  productName: 'Beryllium',
-  customerName: 'Sample',
-  weight: 9.0122,
-  symbol: 'Be',
-  description: `Beryllium is a chemical element with symbol Be and atomic number 4. It is a
-      relatively rare element in the universe, usually occurring as a product of the spallation of
-      larger atomic nuclei that have collided with cosmic rays.`,
-},
-{
-  serialNo: 5,
-  productName: 'Boron',
-  customerName: 'Sample',
-  weight: 10.811,
-  symbol: 'B',
-  description: `Boron is a chemical element with symbol B and atomic number 5. Produced entirely
-      by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a
-      low-abundance element in the Solar system and in the Earth's crust.`,
-},
-{
-  serialNo: 6,
-  productName: 'Carbon',
-  customerName: 'Sample',
-  weight: 12.0107,
-  symbol: 'C',
-  description: `Carbon is a chemical element with symbol C and atomic number 6. It is nonmetallic
-      and tetravalent—making four electrons available to form covalent chemical bonds. It belongs
-      to group 14 of the periodic table.`,
-},
-{
-  serialNo: 7,
-  productName: 'Nitrogen',
-  customerName: 'Sample',
-  weight: 14.0067,
-  symbol: 'N',
-  description: `Nitrogen is a chemical element with symbol N and atomic number 7. It was first
-      discovered and isolated by Scottish physician Daniel Rutherford in 1772.`,
-},
-{
-  serialNo: 8,
-  productName: 'Oxygen',
-  customerName: 'Sample',
-  weight: 15.9994,
-  symbol: 'O',
-  description: `Oxygen is a chemical element with symbol O and atomic number 8. It is a member of
-       the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
-       agent that readily forms oxides with most elements as well as with other compounds.`,
-},
-{
-  serialNo: 9,
-  productName: 'Fluorine',
-  customerName: 'Sample',
-  weight: 18.9984,
-  symbol: 'F',
-  description: `Fluorine is a chemical element with symbol F and atomic number 9. It is the
-      lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard
-      conditions.`,
-},
-{
-  serialNo: 10,
-  productName: 'Neon',
-  customerName: 'Sample',
-  weight: 20.1797,
-  symbol: 'Ne',
-  description: `Neon is a chemical element with symbol Ne and atomic number 10. It is a noble gas.
-      Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about
-      two-thirds the density of air.`,
-},
-];
 
 
 
