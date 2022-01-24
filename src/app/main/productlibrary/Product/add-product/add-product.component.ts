b9d90a848e507655f62006ed177b33188ddd7a25
arun <arunkumar.akr1@gmail.com>
2022-01-24 18:17:22 +0530

    progress commit

--- src/app/main/productlibrary/Product/add-product/add-product.component.ts
+++ src/app/main/productlibrary/Product/add-product/add-product.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {  FormBuilder, FormGroup } from '@angular/forms';
 import { Customer } from 'src/app/models/customer.model';
 import { CustomerApiService } from 'src/app/services/customer-api.service';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MachineApiService } from 'src/app/services/machine-api.service';
+import { ProductApiService } from 'src/app/services/product-api.service';
+import { Product } from 'src/app/models/product.model';
+import { Machine } from 'src/app/models/machine.model';
 
 @Component({
   selector: 'app-add-product',
@@ -11,19 +16,46 @@ import { CustomerApiService } from 'src/app/services/customer-api.service';
 export class AddProductComponent implements OnInit {
  
   customerData : Customer[] = [] ;
-  mapForm1 : FormGroup;
+  productData : Product[] = [];
+  machineData: Machine[] = [];
+  addForm : FormGroup;
+  processForm: FormGroup;
+  detailForm:FormGroup;
+  machineForm:FormGroup;
 
   constructor(
     private customerApi : CustomerApiService,
+    private machineApi : MachineApiService,
+    private productApi : ProductApiService,
     private fb:FormBuilder
   ) { 
     
-    this.mapForm1 = this.fb.group({
+    this.addForm = this.fb.group({
        customerName : '',
        productName:'',
        drawingNo:'',
        revisionNo:''
     })
+
+    this.processForm = this.fb.group({
+       processName : '',
+       processDrawing: ''
+    })
+  
+    this.detailForm = this.fb.group({
+      productName :'',
+      productDrawing:'',
+      processName:'',
+      numberOfProcess:'',
+      processDrawing:''
+    })
+
+    this.machineForm = this.fb.group({
+      processName:'',
+      machineName:''
+
+    })
+
 
   }
 
@@ -32,9 +64,29 @@ export class AddProductComponent implements OnInit {
         this.customerApi.getCustomerAll().subscribe(result =>{
           this.customerData = result ;
         })
+
+        this.productApi.get().subscribe(result =>{
+          this.productData = result ;
+        })
+
+        this.machineApi.getMachineAll().subscribe(result =>{
+          this.machineData = result ; 
+        })
   }
 
-  onSave(){
-    console.log(this.mapForm1.value);
+  onProductSave(){
+    console.log(this.addForm.value);
+  }
+
+  onProcessSave(){
+    console.log(this.addForm.value);
+  }
+
+  onProductDetail(){
+    console.log(this.addForm.value);
+  }
+
+  onMachineMapping(){
+    console.log(this.addForm.value);
   }
 }
--- src/app/main/productlibrary/productlibrary.module.ts
+++ src/app/main/productlibrary/productlibrary.module.ts
@@ -3,11 +3,11 @@ import { ProductLibraryComponent } from "./productlibrary.component";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MaterialModule } from "src/app/material/material.module";
-import {  ProductComponent } from './Product/product.component';
+import {  ProductComponent } from '../productlibrary/Product/product.component';
 import { ProductLibraryRoutingModule } from "./productlibrary-routing.module";
 import { FileUploadComponent } from "./file-upload/file-upload.component";
 import { FlexLayoutModule } from "@angular/flex-layout";
-import { AddProductComponent } from './Product/add-product/add-product.component';
+import { AddProductComponent } from '../productlibrary/Product/add-product/add-product.component';
 
 
 @NgModule({
